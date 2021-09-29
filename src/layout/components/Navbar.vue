e49c65e990911b1cf6dd2d7027bc916d1b6191ec
kuanghua <869653722@qq.com>
2021-09-29 18:20:16 +0800

    fix demo issue

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -26,10 +26,10 @@
             <router-link to="/">
               <el-dropdown-item>Home</el-dropdown-item>
             </router-link>
-            <a target="_blank" href="https://github.com/jzfai/vue3-admin-template">
+            <a target="_blank" href="https://github.com/jzfai/vue3-admin-plus">
               <el-dropdown-item>Github</el-dropdown-item>
             </a>
-            <a target="_blank" href="https://github.com/jzfai/vue3-admin-template">
+            <a target="_blank" href="https://github.com/jzfai/vue3-admin-plus">
               <el-dropdown-item>Docs</el-dropdown-item>
             </a>
             <!--<el-dropdown-item>修改密码</el-dropdown-item>-->
