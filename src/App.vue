aa8e333b03f7764085f1fb709b15d25739526624
jzfai <869653722@qq.com>
2021-12-24 12:04:18 +0800

    add Global var demo

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,25 @@
 <template>
   <router-view />
+  <div class="show-version-container">
+    {{ showVersionInfo }}
+  </div>
 </template>
+
+<script setup>
+import { computed } from 'vue'
+import packageJson from '../package.json'
+const showVersionInfo = computed(() => {
+  return `v${packageJson.version}-${GLOBAL_VAR.GIT_COMMIT_ID}`
+})
+</script>
+
+<style scoped lang="scss">
+.show-version-container {
+  position: fixed;
+  bottom: 6px;
+  right: 6px;
+  background: #fff;
+  padding: 2px 2px;
+  border-radius: 2px;
+}
+</style>
