795cb49a1f832feac13ecc28782cb1b9bdc46398
jzfai <869653722@qq.com>
2022-02-09 11:30:15 +0800

    fix tagView issue

--- src/views/example/parent-children/Parent.vue
+++ src/views/example/parent-children/Parent.vue
@@ -1,15 +1,31 @@
 <template>
   <div class="scroll-y">
     <div>这是父组件</div>
-    <Children ref="refChildren" father-name="Vue3Template" @emitParent="emitParent" />
     <el-button @click="childMethod">childMethod</el-button>
+
+    <!--v-model sync -->
+    <Children ref="refChildren" v-model:childrenTitle="parentTitle" father-name="Vue3Template" @emitParent="emitParent">
+      <!--默认插槽 v-slot -->
+      <template v-slot>
+        <div>默认插槽</div>
+      </template>
+      <!--具名插槽// v-slot:header -->
+      <template #header>
+        <div>具名插槽</div>
+      </template>
+      <!--作用域插槽  //v-slot:footer-->
+      <template #footer="{ testProps }">
+        <div>
+          {{ testProps }}
+        </div>
+      </template>
+    </Children>
   </div>
 </template>
 
 <script setup>
 //script-setup vue官方说明
 //https://v3.cn.vuejs.org/api/sfc-script-setup.html#使用组件
-import { onMounted, getCurrentInstance, ref } from 'vue'
 import Children from './Children.vue'
 //获取store和router
 // import {useRouter} from 'vue-router'
@@ -18,10 +34,7 @@ import Children from './Children.vue'
 const refChildren = ref(null)
 
 onMounted(() => {
-  /*获取子元素两种方法*/
-  //第一种
-  console.log(refChildren.value)
-  //第二种
+  /*获取子元素方法*/
   console.log(refChildren.value)
 })
 const childMethod = () => {
@@ -34,6 +47,20 @@ const emitParent = (data) => {
 const fartherMethod = () => {
   console.log('fartherMethod')
 }
+//provide
+provide('title', 'provide and inject using')
+
+//v-model sync
+const parentTitle = ref('parentTitle')
+
+watch(
+  () => parentTitle.value,
+  (oldValue, newValue) => {
+    console.log('触发parent更新了', oldValue, newValue)
+  },
+  { immediate: true }
+)
+
 defineExpose({ fartherMethod })
 </script>
 
---
+++ src/views/example/parent-children/SubChildren.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="mt-2">
+    sub-children
+    <div>{{ title }}</div>
+  </div>
+  <!--  Teleport  https://www.jianshu.com/p/1ecf5006b1ae-->
+
+  <div class="mt-2">Teleport</div>
+  <!--
+     teleport container
+     attention: the container must have when the teleport render
+     -->
+  <div id="modal-container"></div>
+
+  <!--  to container-->
+  <teleport v-if="modalOpen" to="#modal-container">
+    <div class="modal">
+      <div>
+        to container
+        <el-button @click="modalOpen = false">Close</el-button>
+      </div>
+    </div>
+  </teleport>
+  <!--  to body-->
+  <!--  <teleport v-if="modalOpen" to="body">-->
+  <!--    <div>to body</div>-->
+  <!--  </teleport>-->
+  <el-button @click="showModalOpen">showModalOpen</el-button>
+</template>
+
+<script setup>
+//provide and inject using
+let title = inject('title')
+
+const modalOpen = ref(false)
+
+const showModalOpen = () => {
+  modalOpen.value = true
+}
+</script>
+
+<style scoped lang="scss"></style>
