d109b34a390a10d91826c941871aed40c8f2c262
jzfai <869653722@qq.com>
2022-02-11 11:21:15 +0800

    fix date choose issue

--- src/views/example/keep-alive/KeepAlive.vue
+++ src/views/example/keep-alive/KeepAlive.vue
@@ -3,13 +3,14 @@
     <div>KeepAlive.vue</div>
     <el-form ref="refsearchForm" :inline="true" class="mt-2">
       <el-form-item label-width="0px" label="" prop="errorLog" label-position="left">
-        <el-input v-model="searchForm.errorLog" class="widthPx-150" placeholder="错误日志" />
+        <el-input v-model="searchForm.errorLog" class="widthPx-150" placeholder="input to test keepAlive" />
       </el-form-item>
       <el-form-item label-width="0px" label="" prop="pageUrl" label-position="left">
-        <el-input v-model="searchForm.pageUrl" class="widthPx-150" placeholder="页面路径" />
+        <el-input v-model="searchForm.pageUrl" class="widthPx-150" placeholder="input to test keepAlive" />
       </el-form-item>
     </el-form>
     <el-button type="primary" @click="routerDemoF">to routerDemoF.vue</el-button>
+    <el-button @click="cancelWatch">cancelWatch</el-button>
   </div>
 </template>
 
@@ -25,7 +26,7 @@ let { searchForm } = useCommon()
 let testRef = ref(1)
 //赋值
 testRef.value = 2
-console.log(testRef.value)
+// console.log(testRef.value)
 
 onActivated(() => {
   console.log('onActivated')
@@ -33,6 +34,32 @@ onActivated(() => {
 onDeactivated(() => {
   console.log('onDeactivated')
 })
+
+const $route = useRoute()
+const $store = useStore()
+// cacheGroup为缓存分组  KeepAlive->routerDemoF->routerDemoS
+let cacheGroup = ['KeepAlive', 'routerDemoF', 'routerDemoS']
+const unWatch = watch(
+  () => $route.name,
+  () => {
+    if (!cacheGroup.includes($route.name)) {
+      useCommon()
+        .sleep(300)
+        .then(() => {
+          cacheGroup.forEach((fItem) => $store.commit('app/M_DEL_CACHED_VIEW', fItem))
+        })
+      //remove watch
+      unWatch()
+    }
+  },
+  //deep: true
+  { immediate: true }
+)
+
+const cancelWatch = () => {
+  unWatch()
+}
+
 const routerDemoF = () => {
   //推荐路由跳转根据router的name,这样在你修改路径时，只要不修改name，就没有影响。
   //推荐传递的是query参数，好处是刷新时可以回显，传入的obj对象会反序列化。
--- src/views/example/keep-alive/RouterDemoF.vue
+++ src/views/example/keep-alive/RouterDemoF.vue
@@ -3,17 +3,17 @@
     <h2>RouterDemof.vue</h2>
     <el-form ref="refsearchForm" :inline="true" class="mt-2">
       <el-form-item label-width="0px" label="" prop="errorLog" label-position="left">
-        <el-input v-model="searchForm.errorLog" class="widthPx-150" placeholder="demo1" />
+        <el-input v-model="searchForm.errorLog" class="widthPx-150" placeholder="input to test keepAlive" />
       </el-form-item>
       <el-form-item label-width="0px" label="" prop="pageUrl" label-position="left">
-        <el-input v-model="searchForm.pageUrl" class="widthPx-150" placeholder="demo1" />
+        <el-input v-model="searchForm.pageUrl" class="widthPx-150" placeholder="input to test keepAlive" />
       </el-form-item>
     </el-form>
     <el-button type="primary" @click="routerDemoS">to routerDemoS.vue</el-button>
     <el-button type="primary" @click="routerBack">返回</el-button>
   </div>
 </template>
-<script setup>
+<script setup name="routerDemoF">
 let { searchForm } = useCommon()
 onMounted(() => {
   //get page pass url data
