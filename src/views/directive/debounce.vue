932785fa5f285359a02e91cbdc3ff1b5623f5a38
zfj <844788189@qq.com>
2022-02-17 14:41:43 +0800

    新增自定义指令

---
+++ src/views/directive/debounce.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <el-button v-debounce="getData(123)" type="primary">防抖按钮</el-button>
+  </div>
+</template>
+
+<script setup>
+import { ElMessage } from "element-plus";
+
+const getData = (str) => {
+  return function () {
+    console.log(str);
+    ElMessage({
+      type: "success",
+      message: "正在拉取数据",
+    });
+  };
+};
+onUnmounted(() => {
+  ElMessage.closeAll();
+});
+</script>
+
+<style lang="scss" scoped></style>
