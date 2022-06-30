57d1df6b4cd5181a83a969a3c2c0f20e13c14f59
zfj <844788189@qq.com>
2022-06-30 11:51:08 +0800

    add dragPane

---
+++ src/views/use-example/dragPane/dragPane.vue
@@ -0,0 +1,41 @@
+<template>
+  <splitpanes class="default-theme" horizontal :push-other-panes="false">
+    <pane>
+      <div class="item" style="background-color: #fe5308">1</div>
+    </pane>
+    <pane>
+      <splitpanes :push-other-panes="false">
+        <pane>
+          <div class="item" style="background-color: #fefe34">2</div>
+        </pane>
+        <pane>
+          <div class="item" style="background-color: #cfea2b">3</div>
+        </pane>
+        <pane>
+          <div class="item" style="background-color: #66b032">4</div>
+        </pane>
+      </splitpanes>
+    </pane>
+    <pane>
+      <div class="item" style="background-color: #0492ce">5</div>
+    </pane>
+  </splitpanes>
+</template>
+
+<script setup>
+import { Splitpanes, Pane } from "splitpanes";
+import "splitpanes/dist/splitpanes.css";
+</script>
+
+<style lang="scss" scoped>
+.item {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-family: Helvetica, Arial, sans-serif;
+  color: #333;
+  font-size: 5em;
+  width: 100%;
+  height: 100%;
+}
+</style>
