24996acb28a1f45a1378b8ee7b14c98a229dcde8
zfj <844788189@qq.com>
2022-07-29 11:06:20 +0800

    use Tinymce 6.10

--- src/views/use-example/tinymce-example/TinymceExample.vue
+++ src/views/use-example/tinymce-example/TinymceExample.vue
@@ -3,29 +3,34 @@
     <div class="mb-1">
       <el-button @click="setTinyContent">设置文本消息</el-button>
       <el-button @click="getTinyContent">获取文本消息</el-button>
+      <el-button @click="clearTinyContent">清空内容</el-button>
     </div>
-    <Tinymce ref="refTinymce" />
-    <div class="mt-2">只读,readOnly为true</div>
-    <Tinymce :read-only="true" />
+    <Tinymce ref="refTinymce"  />
   </div>
 </template>
 
 <script setup>
-import Tinymce from '@/components/Tinymce/Tinymce.vue'
+import Tinymce from "@/components/Tinymce/index.vue";
 /*tinymce操作*/
-const refTinymce = ref(null)
+const refTinymce = ref(null);
 const setTinyContent = () => {
   if (refTinymce?.value) {
-    refTinymce.value.setContent('tinymce设置的内容')
+    refTinymce.value.setContent("tinymce设置的内容");
   }
-}
-const { elMessage } = useElement()
+};
+const { elMessage } = useElement();
 const getTinyContent = () => {
   if (refTinymce?.value) {
-    let content = refTinymce.value.getContent()
-    elMessage(content)
+    let content = refTinymce.value.getContent();
+    elMessage(content);
   }
-}
+};
+const clearTinyContent = () => {
+  if (refTinymce?.value) {
+    refTinymce.value.setContent("");
+  }
+};
+
 </script>
 
 <style scoped lang="scss"></style>
