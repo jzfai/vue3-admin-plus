283d289fe0d0a63503a3fe8cf6192ad84c31b03d
ponychoker <ponychoker@163.com>
2022-11-25 11:55:23 +0800

    feat: demo for echarts

--- src/router/modules/charts.ts
+++ src/router/modules/charts.ts
@@ -1,17 +1,42 @@
+/** When your routing table is too long, you can split it into small modules**/
+
 import Layout from '@/layout/index.vue'
-const charts = {
+
+const chartsRouter = {
   path: '/charts',
   component: Layout,
-  meta: { title: 'Charts', icon: 'clipboard' },
-  alwaysShow: true,
+  redirect: 'noRedirect',
+  name: 'Charts',
+  meta: {
+    title: 'Charts',
+    icon: 'chart'
+  },
   children: [
     {
-      path: 'index',
-      component: () => import('@/views/charts/index.vue'),
-      name: 'Index',
-      meta: { title: 'Index' }
+      path: 'keyboard',
+      component: () => import('@/views/charts/keyboard.vue'),
+      name: 'KeyboardChart',
+      meta: { title: 'Keyboard Chart', noCache: true }
+    },
+    {
+      path: 'line',
+      component: () => import('@/views/charts/line.vue'),
+      name: 'LineChart',
+      meta: { title: 'Line Chart', noCache: true }
+    },
+    {
+      path: 'mix-chart',
+      component: () => import('@/views/charts/mix-chart.vue'),
+      name: 'MixChart',
+      meta: { title: 'Mix Chart', noCache: true }
+    },
+    {
+      path: 'echarts-demo',
+      component: () => import('@/views/charts/echarts-demo.vue'),
+      name: 'EchartsDemo',
+      meta: { title: 'Echarts Demo', noCache: true }
     }
   ]
 }
 
-export default charts
+export default chartsRouter
