eb45376a2cb76465ee22f45fb1c7b3d10cc7a21b
jzfai <869653722@qq.com>
2022-12-07 17:34:33 +0800

    dd

--- change-git.sh
+++ change-git.sh
@@ -1,17 +1,17 @@
-git filter-branch --env-filter'
+git filter-branch --env-filter '
 
-oldEmail="869653722@qq.com"
-newName="jzfai"
-newEmail="869653722@qq.com"
+OLD_EMAIL="869653722@qq.com" // 要替换的邮箱
+CORRECT_NAME="jzfai" // 新的用户名
+CORRECT_EMAIL="869653722@qq.com" // 新的邮箱
 
-if [ "$GIT_COMMITTER_EMAIL" = "$oldEmail" ]; then
-    export GIT_COMMITTER_NAME="$newName"
-    export GIT_COMMITTER_EMAIL="$newEmail"
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
 fi
-
-if [ "$GIT_AUTHOR_EMAIL" = "$oldEmail" ]; then
-    export GIT_AUTHOR_NAME="$newName"
-    export GIT_AUTHOR_EMAIL="$newEmail"
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
 fi
-
-' --tag-name-filter cat -- --branches --tags
+'
