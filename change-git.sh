5434cac47e562a5066cad23a2b954422bfcc211d
jzfai <869653722@qq.com>
2022-12-07 17:55:34 +0800

    fix issue

--- change-git.sh
+++ change-git.sh
@@ -1,15 +1,15 @@
-git filter-branch --env-filter '
+git filter-branch -f --env-filter '
 
-OLD_EMAIL="869653722@qq.com" // 要替换的邮箱
-CORRECT_NAME="kuanghua" // 新的用户名
+OLD_EMAIL="869653722qq.com" // 要替换的邮箱
+CORRECT_NAME="jzfai" // 新的用户名
 CORRECT_EMAIL="869653722@qq.com" // 新的邮箱
 
-if [ "$GIT_COMMITTER_NAME" = "$CORRECT_NAME" ]
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
 then
     export GIT_COMMITTER_NAME="$CORRECT_NAME"
     export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
 fi
-if [ "$GIT_COMMITTER_NAME" = "$CORRECT_NAME" ]
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
 then
     export GIT_AUTHOR_NAME="$CORRECT_NAME"
     export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
